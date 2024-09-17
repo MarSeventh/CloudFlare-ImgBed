cbbdbb38327bd7308b7b8b3d8a98eafdfeefeb5c
DipperMorgan <ezioauditorebaobao@gmail.com>
2024-09-17 12:45:42 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -42,7 +42,8 @@ export async function onRequest(context) {  // Contents of context object
     }
     const imgRecord = await env.img_url.getWithMetadata(params.id);
     // 图片是否存在
-    if (imgRecord === null || imgRecord?.metadata === null) {
+    // imgRecord === null || imgRecord?.metadata === null
+    if (imgRecord === null) {
         return new Response('Error: Image not found', { status: 404 });
     }
 
